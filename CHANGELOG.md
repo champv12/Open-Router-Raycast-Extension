c77eb4282defb025875f5ea5f3a8ac1ff937624d
champv12 <kittypaws875@gmail.com>
2025-04-10 13:01:41 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Open Router API Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ src/ask-ai.tsx
@@ -0,0 +1,9 @@
+import { Detail } from "@raycast/api";
+
+interface Props {
+
+}
+
+export default function Command() {
+  return <Detail markdown="# Hello World" />;
+}
